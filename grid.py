8e49a5373e05a9972f520d1678a237b2be348c6e
Daniel <danieljzhang@hotmail.com>
2019-01-27 20:46:25 -0700

    Chapter 3 grid.py

---
+++ grid.py
@@ -0,0 +1,17 @@
+# Chapter 3
+# grid.py
+
+def printline(symbol1, symbol2):
+    print(symbol1, symbol2 * 4, symbol1, symbol2 *4, symbol1)
+
+printline('+', '-')
+printline('+', ' ')
+printline('+', ' ')
+printline('+', ' ')
+printline('+', ' ')
+printline('+', '-')
+printline('+', ' ')
+printline('+', ' ')
+printline('+', ' ')
+printline('+', ' ')
+printline('+', '-')
